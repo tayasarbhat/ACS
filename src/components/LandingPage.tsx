c9f6c37ebdb8307024a747ec3b1881a64b094291
tayasarbhat <tayasarbhat@gmail.com>
2025-02-04 11:08:24 +0530

    Update LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -136,7 +136,7 @@ function LandingPage({ onNavigate }: LandingPageProps) {
               key={tool.id}
               onClick={() => {
                 if (tool.id === 'google-sheets') {
-                  window.open('https://tayasarbhat.github.io/GoogleSheets/', '_blank');
+                  window.open('https://tayasarbhat.github.io/GS/', '_blank');
                 } else {
                   onNavigate(tool.id as any);
                 }
